d96c868a755d618879685ee45ec34b8c3e1611e2
Neeraj Singh <neeraj@bigbinary.com>
2015-07-05 09:53:46 -0400

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 var path = require('path');
+var merge = require('webpack-merge');
+var TARGET = process.env.TARGET;
+var ROOT_PATH = path.resolve(__dirname);
+var common={
+entry: [path.resolve(ROOT_PATH, 'app/main')], output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+}, ],
+}, };
 
-module.exports = {
-  entry: path.resolve(__dirname, 'app/main'),
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  }
+if(TARGET === 'build') { module.exports = common;
+}
+
+if(TARGET === 'dev') { module.exports = merge(common, {
+    entry: [
+      'webpack-dev-server/client?http://0.0.0.0:8080',
+      'webpack/hot/dev-server'
+] });
 }
